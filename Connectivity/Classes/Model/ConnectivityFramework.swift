0f01d30f1f05a89419f8021a1c6ff353aa1f6104
Ross Butler <github@rwbutler.com>
2019-11-13 13:22:21 +0000

    Release 3.3.1

--- Connectivity/Classes/Model/ConnectivityFramework.swift
+++ Connectivity/Classes/Model/ConnectivityFramework.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // MARK: Type definitions
-public enum ConnectivityFramework {
+@objc public enum ConnectivityFramework: Int {
     case network
     case systemConfiguration
 }
