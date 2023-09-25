6e95a1f27afb8ca35fcb79718cf0abd51ce5356f
Zandor Smith <info@zsinfo.nl>
2023-09-25 15:02:51 +0200

    Add visionOS example project for testing.

---
+++ Example/Connectivity_Example_visionOS/Connectivity_Example_visionOSApp.swift
@@ -0,0 +1,18 @@
+//
+//  Connectivity_Example_visionOSApp.swift
+//  Connectivity_Example_visionOS
+//
+//  Created by Zandor Smith on 25/09/2023.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct Connectivity_Example_visionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
